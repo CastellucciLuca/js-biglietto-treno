c68abb8a7e97f8eaa7504807d5e7b0b25b2cb4e8
CastellucciLuca <castellucciluca98@gmail.com>
2022-11-21 15:35:52 +0100

    Js-final-content

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-// INPUT
-const kilometres = prompt ("Quanti chilometri dovrai percorrere?")
+const kiloMetres = parseFloat(prompt("Quanta distanza devi percorrere?")); 
 
-const personAge = prompt ("Quanti anni hai?")
+const personAge = parseInt(prompt("Che età hai?"));
+
+document.getElementById('km').innerHTML = kiloMetres + 'km';
+document.getElementById('eta').innerHTML = personAge + '';
+
+let ticketPrice = kiloMetres *0.21;
+
+let ticketUnder = ticketPrice - (ticketPrice * 0.21);
+let ticketOver = ticketPrice - (ticketPrice * 0.39);
+
+if (personAge <= 18) {
+    document.getElementById('prezzo').innerHTML = 'Il costo del biglietto è di  : &euro; ' + ticketUnder.toFixed(2)
+} else if (personAge >= 65){
+    document.getElementById('prezzo').innerHTML = 'Il costo del biglietto è di : &euro; ' + ticketOver.toFixed(2)
+} else {
+    document.getElementById('prezzo').innerHTML = 'Il costo del biglietto è di : &euro;' + ticketPrice.toFixed(2)
+
+}
